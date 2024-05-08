8a7200e15229c23db31cdf4db1c3693bef39c998
zxrohex <contact@bytestxck.software>
2024-05-08 17:55:22 +0200

    Start work on dynamical scroll-based animation

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,16 @@
+jQuery.fn.extend({
+	scrollProgress: function () {
+		return ($(window).scrollTop() - $(this).position().top) / ($(this).height());
+	}
+});
+
+
 $(function () {
 	$(window).on("scroll", function () {
-		const scrollProgress = $(window).scrollTop() / ($("#test3").height() - $("#test3").innerHeight());
+		$(".scroll-vars").each(function () {
+			var scrollProgress = $(this).scrollProgress();
 
-		// Map progress to the degree range
-		var degrees = 40 + (scrollProgress * 80); // 80 = (120 - 40)
-
-		$("#test2").css("transform", "rotateX(" + degrees + "deg)");
+			$(this).css("--scroll-deg", scrollProgress +  "deg");
+		});
 	});
 });
