029c7c8db7f9765c27bf4c2e37983c9c525dece4
zxrohex <contact@bytestxck.software>
2024-05-14 06:41:33 +0200

    Re-work window menu, some additional work

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,28 @@
 $(function() {
     $(".shell-window").draggable({
         handle: ".window-titlebar"
+    }).resizable({
+        handles: "se"
     });
+
+    $(window).on("keydown", function (e) {
+        e.preventDefault();
+
+        if (e.ctrlKey && e.shiftKey && e.keyCode === 72) {
+
+            $("div.desktop-shell").css({
+                "background-image": "var(--b64-img)",
+                "background-size": "cover"
+            });
+        }
+
+    });
+
+    /*$("[data-testrole='close']").on("click", function () {
+        $(this).closest(".shell-window").remove();
+    });
+
+    $("button.window-menu").on("click", function () {
+       console.log($(this).parent().closest("window-menu-container"));
+    });*/
 });
